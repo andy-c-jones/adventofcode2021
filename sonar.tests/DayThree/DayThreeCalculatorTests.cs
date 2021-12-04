50a616e313a34f5b39f0b8e455145c07a57d0966
Andrew Jones <aj@darkpeak.dev>
2021-12-04 21:17:52 +0000

    Calculate power consumption

--- sonar.tests/DayThree/DayThreeCalculatorTests.cs
+++ sonar.tests/DayThree/DayThreeCalculatorTests.cs
@@ -15,6 +15,27 @@ public class DayThreeCalculatorTests
         Assert.That(powerConsumption, Is.EqualTo(0));
     }
 
+    [TestCase(new[] {"00001", "00001", "00001"}, 30)]
+    [TestCase(new[] {"00001", "00001", "00001", "00001"}, 30)]
+    [TestCase(new[] {"00000", "00000", "00001", "00001"}, 30)]
+    [TestCase(new[] {"00000", "00000", "00001", "00001", "00001"}, 30)]
+    [TestCase(new[] {"00000", "00001", "00001"}, 30)]
+    [TestCase(new[] {"00001", "00001", "00000"}, 30)]
+    [TestCase(new[] {"00010", "00000", "00000"}, 31)]
+    [TestCase(new[] {"00010", "00010", "00000"}, 29)]
+    [TestCase(new[] {"00010", "00010", "00010"}, 29)]
+    [TestCase(new[] {"00100", "00100", "00010"}, 27)]
+    [TestCase(new[] {"01100", "01100", "00010"}, 19)]
+    [TestCase(new[] {"11100", "11100", "00010"}, 3)]
+    public void
+        Epsilon_rate_is_int_parsed_from_the_binary_found_from_identifying_all_the_least_common_bits_across_values(
+            string[] inputs, int expectedGammaRate)
+    {
+        var calculator = new DayThreeCalculator();
+        var gammaRate = calculator.CalculateEpsilonRate(inputs);
+        Assert.That(gammaRate, Is.EqualTo(expectedGammaRate));
+    }
+
     [TestCase(new[] {"00001", "00001", "00001"}, 1)]
     [TestCase(new[] {"00001", "00001", "00001", "00001"}, 1)]
     [TestCase(new[] {"00000", "00000", "00001", "00001"}, 0)]
@@ -33,5 +54,14 @@ public class DayThreeCalculatorTests
         var calculator = new DayThreeCalculator();
         var gammaRate = calculator.CalculateGammaRate(inputs);
         Assert.That(gammaRate, Is.EqualTo(expectedGammaRate));
+    }
+
+    [TestCase(new[] {"00001", "00001", "00001"}, 30)]
+    [TestCase(new[] {"00010", "00010", "00010"}, 58)]
+    public void Power_consumption_should_be_gamma_rate_times_epsilon_rate(string[] inputs, int expectedPowerConsumption)
+    {
+        var calculator = new DayThreeCalculator();
+        var gammaRate = calculator.CalculatePowerConsumption(inputs);
+        Assert.That(gammaRate, Is.EqualTo(expectedPowerConsumption));
     }
 }
--- sonar/DayThree/DayThreeCalculator.cs
+++ sonar/DayThree/DayThreeCalculator.cs
@@ -2,36 +2,28 @@
 
 public class DayThreeCalculator : IDayThreeCalculator
 {
-    public int CalculatePowerConsumption(string[] input)
-    {
-        return 0;
-    }
+    public int CalculatePowerConsumption(string[] input) =>
+        CalculateGammaRate(input) * CalculateEpsilonRate(input);
 
-    public int CalculateGammaRate(string[] input)
-    {
-        var inputLength = input.Length;
+    public int CalculateGammaRate(string[] input) => Calculate(input, Common);
+    public int CalculateEpsilonRate(string[] input) => Calculate(input, LeastCommon);
 
+
+    private static int Calculate(IReadOnlyCollection<string> input, Func<char, bool> countingFunction)
+    {
         var result = "";
-
         for (var i = 0; i < 5; i++)
         {
             var column = input.Aggregate("", (current, s) => current + s[i]);
-
-            var count = column.Count(c => c == '1');
-            var halfRoundedDown = inputLength / 2;
-            if (count >= halfRoundedDown + 1)
-            {
-                result += "1";
-            }
-            else
-            {
-                result += "0";
-            }
+            var count = column.Count(countingFunction);
+            result += count >= input.Count / 2 + 1 ? "1" : "0";
         }
 
         return Convert.ToInt32(result, 2);
-        ;
     }
+
+    private static bool Common(char c) => c == '1';
+    private static bool LeastCommon(char c) => c == '0';
 }
 
 public interface IDayThreeCalculator
