6bf2865da1de0dcd2bcc597e0bc49b3ef2600abd
Andrew Jones <aj@darkpeak.dev>
2021-12-04 16:37:44 +0000

    renaming console commands for day 1-3

--- README.md
+++ README.md
@@ -8,9 +8,15 @@ Run `./precommit.sh`
 
 # To run with input file
 
-To calculate depth increases:
-`dotnet sonar/bin/Debug/net6.0/sonar.dll sonar "./sonar/bin/Debug/net6.0/day1Input.txt"`
 
+## Day One
+To calculate depth increases:
+`dotnet sonar/bin/Debug/net6.0/sonar.dll day1 "./sonar/bin/Debug/net6.0/day1Input.txt"`
+
+## Day Two
 To move a submarine:
-`dotnet sonar/bin/Debug/net6.0/sonar.dll movesub "./sonar/bin/Debug/net6.0/day2Input.txt"`
+`dotnet sonar/bin/Debug/net6.0/sonar.dll day2 "./sonar/bin/Debug/net6.0/day2Input.txt"`
+
+## Day Three
+`dotnet sonar/bin/Debug/net6.0/sonar.dll day3 "./sonar/bin/Debug/net6.0/day3Input.txt"`
 
--- sonar/Program.cs
+++ sonar/Program.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using sonar.DayOne;
+using sonar.DayThree;
 using sonar.DayTwo;
 
 namespace sonar;
@@ -10,21 +11,27 @@ public static class Program
     public static async Task Main(string[] args)
     {
         var serviceProvider = new ServiceCollection()
-            .AddSingleton<IDepthIncreaseCalculator, DepthIncreaseCalculator>()
-            .AddSingleton<ISonarReader, SonarReader>()
             .AddSingleton<IOutputWriter, ConsoleWriter>()
+            .AddSingleton<ISonarReader, SonarReader>()
+            .AddSingleton<IDepthIncreaseCalculator, DepthIncreaseCalculator>()
             .AddSingleton<ICommandReader, CommandReader>()
+            .AddSingleton<IDayThreeCalculator, DayThreeCalculator>()
+            .AddSingleton<IDayThreeReader, DayThreeReader>()
             .AddSingleton<DayOneRunner>()
             .AddSingleton<DayTwoRunner>()
+            .AddSingleton<DayThreeRunner>()
             .BuildServiceProvider();
         
         switch (args[0])
         {
-            case "sonar":
+            case "day1":
                 await serviceProvider.GetService<DayOneRunner>()?.Run(args)!;
                 break;
-            case "movesub":
+            case "day2":
                 await serviceProvider.GetService<DayTwoRunner>()?.Run(args)!;
+                break;
+            case "day3":
+                await serviceProvider.GetService<DayThreeRunner>()?.Run(args)!;
                 break;
         }
     }
