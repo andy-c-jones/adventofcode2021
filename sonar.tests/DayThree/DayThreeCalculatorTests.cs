0b638c1963854ed56682819b2345255a418b7615
Andrew Jones <aj@darkpeak.dev>
2021-12-04 17:04:24 +0000

    Calculate gamma rate

---
+++ sonar.tests/DayThree/DayThreeCalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using sonar.DayThree;
+
+namespace sonar.tests.DayThree;
+
+public class DayThreeCalculatorTests
+{
+    [Test]
+    public void Should_handle_empty_input()
+    {
+        var calculator = new DayThreeCalculator();
+
+        var powerConsumption = calculator.CalculatePowerConsumption(Array.Empty<string>());
+
+        Assert.That(powerConsumption, Is.EqualTo(0));
+    }
+
+    [TestCase(new[] {"00001", "00001", "00001"}, 1)]
+    [TestCase(new[] {"00001", "00001", "00001", "00001"}, 1)]
+    [TestCase(new[] {"00000", "00000", "00001", "00001"}, 0)]
+    [TestCase(new[] {"00000", "00000", "00001", "00001", "00001"}, 1)]
+    [TestCase(new[] {"00000", "00001", "00001"}, 1)]
+    [TestCase(new[] {"00001", "00001", "00000"}, 1)]
+    [TestCase(new[] {"00010", "00000", "00000"}, 0)]
+    [TestCase(new[] {"00010", "00010", "00000"}, 2)]
+    [TestCase(new[] {"00010", "00010", "00010"}, 2)]
+    [TestCase(new[] {"00100", "00100", "00010"}, 4)]
+    [TestCase(new[] {"01100", "01100", "00010"}, 12)]
+    [TestCase(new[] {"11100", "11100", "00010"}, 28)]
+    public void Gamma_rate_is_int_parsed_from_the_binary_found_from_identifying_all_the_common_bits_across_values(
+        string[] inputs, int expectedGammaRate)
+    {
+        var calculator = new DayThreeCalculator();
+        var gammaRate = calculator.CalculateGammaRate(inputs);
+        Assert.That(gammaRate, Is.EqualTo(expectedGammaRate));
+    }
+}
--- sonar/DayThree/DayThreeCalculator.cs
+++ sonar/DayThree/DayThreeCalculator.cs
@@ -4,7 +4,33 @@ public class DayThreeCalculator : IDayThreeCalculator
 {
     public int CalculatePowerConsumption(string[] input)
     {
-        throw new NotImplementedException();
+        return 0;
+    }
+
+    public int CalculateGammaRate(string[] input)
+    {
+        var inputLength = input.Length;
+
+        var result = "";
+
+        for (var i = 0; i < 5; i++)
+        {
+            var column = input.Aggregate("", (current, s) => current + s[i]);
+
+            var count = column.Count(c => c == '1');
+            var halfRoundedDown = inputLength / 2;
+            if (count >= halfRoundedDown + 1)
+            {
+                result += "1";
+            }
+            else
+            {
+                result += "0";
+            }
+        }
+
+        return Convert.ToInt32(result, 2);
+        ;
     }
 }
 
